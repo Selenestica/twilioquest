7981dcd579d0bc3d7a0e952fc06cf224aa46464a
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-05-23 22:00:17 -0500

    finished ducktypium but it doesnt pass for some reason??

--- ducktypium.js
+++ ducktypium.js
@@ -1,9 +1,51 @@
 class Ducktypium {
   constructor(color) {
-    if (color === "red" || color === "blue" || color === "yellow") {
-      this.color = color;
-    } else {
-      console.log("Error!");
+    try {
+      if (color === "red" || color === "blue" || color === "yellow") {
+        this.color = color;
+      } else {
+        throw new Error("Color must be red, yellow, or blue!");
+      }
+    } catch (e) {
+      console.log(e);
     }
+    this.calibrationSequence = [];
+  }
+  refract(color) {
+    try {
+      if (color === "red" || color === "blue" || color === "yellow") {
+        if (color === this.color) {
+          return color;
+        } else if (
+          (color === "red" && this.color === "blue") ||
+          (color === "blue" && this.color === "red")
+        ) {
+          return "purple";
+        } else if (
+          (color === "yellow" && this.color === "blue") ||
+          (color === "blue" && this.color === "yellow")
+        ) {
+          return "green";
+        } else if (
+          (color === "yellow" && this.color === "red") ||
+          (this.color === "yellow" && color === "red")
+        ) {
+          return "orange";
+        }
+      } else {
+        throw new Error("Color must be red, yellow, or blue!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  calibrate(numArray) {
+    let newArray = [];
+    for (let i = 0; i < numArray.length; i++) {
+      let newNum = numArray[i] * 3;
+      newArray.push(newNum);
+    }
+    const sortedArray = newArray.sort((a, b) => a - b);
+    this.calibrationSequence = sortedArray;
   }
 }
